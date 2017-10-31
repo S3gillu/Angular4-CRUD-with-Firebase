458505e1420c185fe2380f71cea0b1e6d2b3843c
S3gillu <shubhamsaurabh440@yahoo.com>
2017-10-31 18:59:46 +0530

    css applied

---
+++ src/app/employees/shared/employee.model.ts
@@ -0,0 +1,7 @@
+export class Employee {
+    $key: string;               // It is like a primary key in sql server for the firebase
+    name: string;
+    position: string;
+    office: string;
+    salary: number;
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyD_2t0lRZlu8xZmcqnZZQtjQBTsyNb3J-8",
+    authDomain: "employeeregister-a481e.firebaseapp.com",
+    databaseURL: "https://employeeregister-a481e.firebaseio.com",
+    projectId: "employeeregister-a481e",
+    storageBucket: "employeeregister-a481e.appspot.com",
+    messagingSenderId: "950511886329"
+  }
 };
